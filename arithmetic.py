b35fd924c3b996563e39966e4de9cbda31dfc4d5
Aileen Liu <aileenliu184@gmail.com>
2019-07-09 21:31:52 -0700

    Make math function work properly

---
+++ arithmetic.py
@@ -0,0 +1,29 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num2 - num1
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1/num2
+
+def square(num1):
+    return num1 * num1
+
+def cube(num1):
+    return num1 * num1 * num1
+
+def power(num1, num2):
+    return num1 ** num2
+
+def mod(num1, num2):
+    return num1 % num2
